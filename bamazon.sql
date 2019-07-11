b044c78dc405e33fe8231a80458fab3e08d0d9ce
J <j.mey.r8@gmail.com>
2019-07-10 23:32:53 -0400

    added table for products & font colors

--- bamazon.sql
+++ bamazon.sql
@@ -12,13 +12,13 @@ CREATE TABLE products(
 );
 
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES  ("PS4", "Games", 299.99, 20),
-        ("Nintendo Switch", "Games", 299.99, 25),
-        ("Jordan XIV", "Clothing", 180.00, 50),
-        ("Samsung Galaxy S10 Plus", "Phones", 799.99, 15),
+VALUES  ("Playstation 4", "Game Console", 299.99, 20),
+        ("Nintendo Switch", "Game Console", 299.99, 25),
+        ("Jordan XIV", "Clothing", 179.99, 50),
+        ("Samsung Galaxy S10+", "Phones", 799.99, 15),
         ("65 inch TV", "HDTV", 599.99, 10),
-        ("NFL Jersey", "Clothing", 150.00, 14),
-        ("King Size Pillow", "Bedding", 15.00, 50),
+        ("NFL Jersey", "Clothing", 149.99, 14),
+        ("King Size Pillow", "Bedding", 15.99, 50),
         ("Bluetooth headphone", "Electronics", 31.99, 40),
         ("Hennessy 750ml", "Alcohol", 26.99, 25),
         ("Eloquent JavaScript", "Books", 28.51, 65);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require("cli-table");
+var colors = require("colors");
 
 
 var connection = mysql.createConnection({
@@ -14,21 +16,25 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("Thank you for visiting Bamazon! What would you like to purchase from our inventory?" + "\n");
+    console.log("-----------------------------------------------------------------------------------".cyan);
+    console.log("Thank you for visiting Bamazon! What would you like to purchase from our inventory?".bold);
+    console.log("-----------------------------------------------------------------------------------".cyan);
     start();
 });
 
 function start() {
     connection.query("SELECT * FROM products", function(err, res){
         if (err) throw err;
+
+        var table = new Table({
+            head: ["ID".blue, "Product".blue, "Department".blue, "Price".blue, "In Stock".blue],
+            colWidths:[5, 23, 15, 10, 10 ]
+        });
         for (var i = 0; i < res.length; i++){
-            console.log("ID: " + res[i].id);
-            console.log("Product: " + res[i].product_name);
-            console.log("Department: " + res[i].department_name);
-            console.log("Price: $" + res[i].price);
-            console.log("Quantity in stock: " + res[i].stock_quantity);
-            console.log("-----------------------")
-        }
+            table.push([res[i].id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+            )
+        };
+        console.log(table.toString());
         pickItemId();
     });
 };
@@ -43,37 +49,33 @@ function pickItemId(){
         {
             name: "quantity",
             type: "input",
-            message: "How many would you like to purchase?"
+            message: "How many would you like to purchase?" 
 
         }])
         .then(function(answer){
             var idPicked = answer.pickId;
             var quantityPicked = answer.quantity;
-            
-            //console.log("Product ID: " + answer.pickId);
-            //console.log("Quantity chosen: " + answer.quantity);
-            
             orderItems(idPicked, quantityPicked);
         });              
 };
 
-function orderItems(pickId, quantityNeeded) {
-    
+function orderItems(pickId, quantityNeeded) {  
     connection.query("SELECT * FROM products WHERE id = " + pickId, function(err, res) {
         if (err) throw err;
         if(quantityNeeded <= res[0].stock_quantity) {
             var costTotal = res[0].price * quantityNeeded;
-            console.log("Your total cost is " + costTotal + ", for " + quantityNeeded + " " + res[0].product_name + "(s).");
-            console.log("------------------------------------------------------------------------------------------------------");
+            console.log("---------------------------------------------------------------------".cyan);
+            console.log("Your total cost is " + ("$" + costTotal.toFixed(2)).green + ", for (" + quantityNeeded + ") " + res[0].product_name + "(s).");
+            console.log("---------------------------------------------------------------------".cyan);
             connection.query("UPDATE products SET stock_quantity = stock_quantity - " + quantityNeeded + " WHERE id = " + pickId);
         }
         else {
-            console.log("Sorry insufficient quantity or out of stock! Please change total quantity or product ID and try again.");
-            console.log("------------------------------------------------------------------------------------------------------");
+            console.log("------------------------------------------------------------------------------------------------------".cyan);
+            console.log("Sorry insufficient quantity or out of stock! Please change total quantity or product ID and try again.".red);
+            console.log("------------------------------------------------------------------------------------------------------".cyan);
             //start();
         }
        start();    
-    });
-    
+    });  
 };
 
